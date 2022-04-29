c5ccea63d36302d85a8ffca4979e228c65accd73
Kesav-Bobba <bobbak@oregonstate.edu>
2022-04-29 02:01:00 -0700

    updated

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -5,6 +5,7 @@ import org.junit.Assert.*;
 
 
 public class SkillDemoTest {
+    @Test
     public void MultiplyTest(){
         assertEquals(1, SkillDemo.multiply(1, 1)); //a test that passes
     }
