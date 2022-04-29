2688423d8fa7f24bb5884a9a0993cbe6bae5edf9
Kesav-Bobba <bobbak@oregonstate.edu>
2022-04-29 01:47:17 -0700

    It has been updated

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -6,6 +6,6 @@ import org.junit.Assert.*;
 
 public class SkillDemoTest {
     public void MultiplyTest(){
-        assertEquals(6, SkillDemo.multiply(2, 3)); //a test that passes
+        assertEquals(1, SkillDemo.multiply(1, 1)); //a test that passes
     }
 }
