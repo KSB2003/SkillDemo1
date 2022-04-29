3bbadbad3f2c3b41d9efc69de316bfd8824ff656
Kesav-Bobba <bobbak@oregonstate.edu>
2022-04-29 01:44:33 -0700

    Made the changes

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -6,6 +6,6 @@ import org.junit.Assert.*;
 
 public class SkillDemoTest {
     public void MultiplyTest(){
-        assertEquals(1, SkillDemo.multiply(2, 3));
+        assertEquals(6, SkillDemo.multiply(2, 3)); //a test that passes
     }
 }
